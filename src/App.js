cd7ce696dce1123bec0a0d13db69355b39040ded
akashbhosale3915 <70402292+akashbhosale3915@users.noreply.github.com>
2022-01-07 09:22:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
-const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const day = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 const date = `${day[current.getDay()]} ${current.getDate()}th ${monthNames[current.getMonth()]} ${current.getFullYear()}`;
 

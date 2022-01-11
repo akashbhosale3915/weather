883369bb3214fcf56a3a67af4aa16dc19a1722a6
Akash Bhosale <70402292+akashbhosale3915@users.noreply.github.com>
2022-01-11 17:26:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,106 @@
-import { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-let apikey = 'f8520a623608c0dd76cddb2116c822cb'
+let apikey = "f8520a623608c0dd76cddb2116c822cb";
 const current = new Date();
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
-const day = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-const date = `${day[current.getDay()]} ${current.getDate()}th ${monthNames[current.getMonth()]} ${current.getFullYear()}`;
-
+const date = `${day[current.getDay()]} ${current.getDate()}th ${
+  monthNames[current.getMonth()]
+} ${current.getFullYear()}`;
 
 function App() {
+  const [city, setcity] = useState("");
+  const [weather, setweather] = useState({});
 
-  const [city, setcity] = useState('')
-  const [weather, setweather] = useState({})
-
-  const handlecity = e => {
+  const handlecity = (e) => {
     setcity(e.target.value);
-  }
+  };
 
   const handleweather = (e) => {
-    e.preventDefault()
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apikey}`
+    e.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apikey}`;
 
-    axios.get(url)
+    axios
+      .get(url)
       .then((response) => {
-        setweather(response.data)
-        setcity('')
-        console.log(weather)
+        setweather(response.data);
+        setcity("");
+        console.log(weather);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
-
+        console.log(error);
+      });
+  };
 
   return (
-    <div className='maindiv'>
-      <div className='overlay'>
-        <div className='seconddiv'>
+    <div className="maindiv">
+      <div className="overlay">
+        <div className="seconddiv">
           <form onSubmit={handleweather}>
-            <input type='text' placeholder='Search...' value={city} onChange={handlecity} required />
-            <input type='submit' value='Search' />
+            <input
+              type="text"
+              placeholder="Search..."
+              value={city}
+              onChange={handlecity}
+              required
+            />
+            <input type="submit" value="Search" />
           </form>
         </div>
 
-        {
-          (typeof weather.main != 'undefined') ? (
-            <div>
-              <div className='dynamic'>
-                <div className='city'><h1>{weather.name}</h1></div>
-                <div className='date'><h3>{date}</h3></div>
-                <div className='degree'><h1>{Math.round(weather.main.temp)}&deg;C</h1></div>
-                   
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="dynamic">
+              <div className="city">
+                <h1>{weather.name}</h1>
+              </div>
+              <div className="date">
+                <h3>{date}</h3>
+              </div>
+              <div className="degree">
+                <h1>{Math.round(weather.main.temp)}&deg;C</h1>
+              </div>
+              <div className="desc">
+                <img
+                  src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                  alt="img"
+                />
+                <p>{weather.weather[0].description}</p>
               </div>
             </div>
-          ) : (<div className='nodata'><h1>Enter a Location</h1></div>)
-        }
+          </div>
+        ) : (
+          <div className="nodata">
+            <h1>Enter a Location</h1>
+          </div>
+        )}
       </div>
-    </div >
+    </div>
   );
 }
 
